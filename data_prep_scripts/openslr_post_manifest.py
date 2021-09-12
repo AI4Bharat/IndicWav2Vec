9cd98c44d4980317fa2c365926cb1d4f45289e19
tahirjmakhdoomi <68889901+tahirjmakhdoomi@users.noreply.github.com>
2021-09-12 17:16:27 +0530

    Update openslr_post_manifest.py

--- data_prep_scripts/openslr_post_manifest.py
+++ data_prep_scripts/openslr_post_manifest.py
@@ -1,12 +1,5 @@
 basepath = "<path to dataset>"
 
-with open(basepath+"/**/eval_bg/wav.scp") as f:
-    lines = f.read().strip().split('\n')
-for line in tqdm.tqdm(lines):
-#     name, _ = line.strip().split('\t')
-    name = line.strip().split(' ')[0]
-    shutil.copy(basepath+"/audio/"+name+".flac", basepath+"/**/eval_wav/"+name+".flac")
-
 with open(basepath+"/**/train_bg/wav.scp") as f:
     lines = f.read().strip().split('\n')
 for line in tqdm.tqdm(lines):
