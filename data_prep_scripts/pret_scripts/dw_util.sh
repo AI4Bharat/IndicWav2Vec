a944fe6f5cb8787b264206cdb3c491cb9c04d2fd
Tahir <68889901+tahirjmakhdoomi@users.noreply.github.com>
2021-12-24 09:45:54 +0530

    Update dw_util.sh

--- data_prep_scripts/pret_scripts/dw_util.sh
+++ data_prep_scripts/pret_scripts/dw_util.sh
@@ -14,7 +14,7 @@ if [ -d $save_dir ]
 then mkdir -p $save_dir 
 fi
 
-cat $1 | xargs -I '{}' -P $nt yt-dlp -f "bestaudio/best" -ciw -o "$save_dir/%(id)s.%(ext)s" --extract-audio --audio-format wav --audio-quality 0 --no-playlist {} --ppa "ffmpeg:-ac 1 -ar 16000" #--quiet &&
+cat $1 | xargs -I '{}' -P $nt yt-dlp -f "bestaudio/best" -ciw -o "$save_dir/%(id)s.%(ext)s" --extract-audio --audio-format wav --audio-quality 0 --no-playlist https://youtu.be/{} --ppa "ffmpeg:-ac 1 -ar 16000" #--quiet &&
 
 python vad.py "$root/raw_data/" "$root/data_refined/" $folder &&
 python snr_filter.py "$root/data_refined/" $folder "$root/snr_rejected/"&&
